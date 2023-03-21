77505d549f0607d41259e618ebadea94fd5fec71
Kostya <kosg@mail.ru>
2023-03-21 21:42:18 +0300

    add task 27

---
+++ task 27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// 452 -> 11 
+// 82 -> 10
+// 9012 -> 12
+
+int f(int n)
+{
+  int sum = 0;
+  while (n > 0)
+  {
+    sum = sum + n%10;
+    n = n/10;
+  }  
+  return sum;
+}
+Console.Clear();
+Console.Write("inter a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
