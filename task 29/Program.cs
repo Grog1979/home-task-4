a35f44a54dd8edff05f1c25c109755a3b30755fb
Kostya <kosg@mail.ru>
2023-03-21 21:48:08 +0300

    add task 27

---
+++ task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет создавать массив из 8 символов и заполнять значениями от пользователя
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// вводит массив из 8 элементов заполненый 0 и 1 в случайном порядке
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2);
+}
+
+Console.Clear();
+int[] array = new int[8];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
